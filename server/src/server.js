46f6ea8d8a79367c85a05ccc6416c216e73017d4
Pierre Gronnier <pierre.gronnier@gmail.com>
2026-02-27 15:53:15 +0100

    add vercel config

--- server/src/server.js
+++ server/src/server.js
@@ -2,6 +2,11 @@ require("dotenv").config();
 const app = require("./app");
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+module.exports = app;
